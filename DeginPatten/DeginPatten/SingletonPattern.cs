611efcfba817641b1a4e288e4f9640ade0c10d0a
sdf8036 <sdf8036@naver.com>
2020-07-13 09:26:45 +0900

    2

---
+++ DeginPatten/DeginPatten/SingletonPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeginPatten
+{
+    class SingletonPattern
+    {
+    }
+    public sealed class Singleton
+    {
+        public static readonly Singleton Instance = new Singleton();
+
+        private Singleton() { }
+
+        public void Method()
+        {
+            Console.WriteLine(".,..");
+        }
+    }
+
+    class Client3
+    {
+        public static void HowToTest()
+        {
+            Singleton.Instance.Method();
+        }
+    }
+}
---
+++ DeginPatten/DeginPatten/SingletonPattern2.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DeginPatten
+{
+    class SingletonPattern2
+    {
+    }
+
+    public sealed class Configuration
+    {
+        public static Configuration Settings { get; } = new Configuration();
+
+        private Dictionary<String, object> dict = new Dictionary<string, object>();
+
+        private Configuration()
+        {
+            LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
+            var str = File.ReadAllText("Config.json");
+            JObject jo = JObject.Parse(str);
+
+            foreach (var kv in jo)
+            {
+                dict.Add(kv.Key, kv.Value);
+            }
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                return dict[key];
+            }
+        }
+    }
+
+    class Client4
+    {
+        public static void HowToTest()
+        {
+            var user = Configuration.Settings["Username"];
+            var server = Configuration.Settings["Server"];
+            Console.WriteLine($"{server},{user}");
+        }
+    }
+}
