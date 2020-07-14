0b3134b0c2cdd85a2caf3c5d206b3ed92dfa4260
sdf8036 <sdf8036@naver.com>
2020-07-14 15:53:28 +0900

    Obectpoll

---
+++ DeginPatten/DeginPatten/ObjectpollPattern.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeginPatten
+{
+    class ObjectpollPattern
+    {
+    }
+    public class MyConnection
+    {
+        public MyConnection()
+        {
+
+        }
+    }
+
+    public class MyConnectionPool
+    {
+        private readonly ConcurrentBag<MyConnection> pool = new ConcurrentBag<MyConnection>();
+
+        public MyConnection GetObject()
+        {
+            MyConnection obj;
+            if(pool.TryTake(out obj))
+            {
+                return obj;
+            }
+            else
+            {
+                return new MyConnection();
+            }
+        }
+
+        public void ReleaseObject(MyConnection conn)
+        {
+            pool.Add(conn);
+            Debug.WriteLine($"Release: {conn.GetHashCode()}");
+        }
+    }
+
+    class Client5
+    {
+        public static void HowToUse()
+        {
+            var myPool = new MyConnectionPool();
+            MyConnection conn = myPool.GetObject();
+            myPool.ReleaseObject(conn);
+
+            MyConnection conn2 = myPool.GetObject();
+            myPool.ReleaseObject(conn2);
+        }
+    }
+}
