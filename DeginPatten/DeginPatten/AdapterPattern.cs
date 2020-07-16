42aae328c0e6d187322d53407142429d8ad46cfe
sdf8036 <sdf8036@naver.com>
2020-07-16 17:08:41 +0900

    AdapterPattern Upload

---
+++ DeginPatten/DeginPatten/AdapterPattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeginPatten
+{
+    class AdapterPattern
+    {
+    }
+    public class Adapter_Client
+    {
+        public void Operation()
+        {
+            Adapter adapter = new Adapter();
+            adapter.Request();
+        }
+    }
+    public class Adapter
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public void Request()
+        {
+            adaptee.AdapteeRequest();
+        }
+    }
+
+    public class Adaptee
+    {
+        public void AdapteeRequest()
+        {
+
+        }
+    }
+}
