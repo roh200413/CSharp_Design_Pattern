17a354539cad7ce98b38869667cd933872dfa4f3
sdf8036 <sdf8036@naver.com>
2020-07-21 14:42:01 +0900

    DecorationPattern Upload

---
+++ DeginPatten/DeginPatten/DecoratorPattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeginPatten
+{
+    class DecoratorPattern
+    {
+    }
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics g)
+    }
+}
