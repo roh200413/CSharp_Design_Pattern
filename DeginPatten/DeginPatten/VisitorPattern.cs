d66fa79de9fa13402269eadc265162f0fd54cb9e
sdf8036 <sdf8036@naver.com>
2020-07-27 11:48:51 +0900

    VisitorPattern Upload

---
+++ DeginPatten/DeginPatten/VisitorPattern.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeginPatten
+{
+    class VisitorPattern
+    {
+    }
+
+    public interface Element
+    {
+        void Accept(Visitor visitor);
+    }
+    public class ConcreteElementA : Element
+    {
+        public void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+    public class ConcreteElementB : Element
+    {
+        public void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public interface Visitor
+    {
+        void Visit(ConcreteElementA element);
+        void Visit(ConcreteElementB element);
+    }
+
+    public class ConcreteVisitor : Visitor
+    {
+        public void Visit(ConcreteElementA element)
+        {
+            Console.WriteLine(element.ToString());
+        }
+
+        public void Visit(ConcreteElementB element)
+        {
+            Console.WriteLine(element.ToString());
+        }
+    }
+}
