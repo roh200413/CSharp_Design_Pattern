0e21a6663e5f377a3a5a38af3b08dfc90b7feea3
sdf8036 <sdf8036@naver.com>
2020-07-30 16:20:56 +0900

    ObserverPattern Upload

---
+++ DeginPatten/DeginPatten/ObserverPattern.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeginPatten
+{
+    class ObserverPattern
+    {
+    }
+
+    class Observer_Subject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void AddObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var o in observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    interface IObserver
+    {
+        void Update();
+    }
+
+    class ObserverA : IObserver
+    {
+        public void Update()
+        {
+            //update ObserverA object
+        }
+    }
+
+    class ObserverB : IObserver
+    {
+        public void Update()
+        {
+            //update ObserverB object
+        }
+    }
+}
