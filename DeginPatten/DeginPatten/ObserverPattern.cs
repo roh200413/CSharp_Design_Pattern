279a8c6f87a8638baf88b04a5fda3726587d1f0e
sdf8036 <sdf8036@naver.com>
2020-07-31 15:39:59 +0900

    ObserverPattern Upload

--- DeginPatten/DeginPatten/ObserverPattern.cs
+++ DeginPatten/DeginPatten/ObserverPattern.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace DeginPatten
 {
@@ -46,6 +49,51 @@ namespace DeginPatten
         public void Update()
         {
             //update ObserverB object
+        }
+    }
+
+
+
+
+  ///////////////////////////////////////////////////////////////
+  
+
+    class ObserverSubject
+    {
+        public event EventHandler Changed;
+
+        public ObserverSubject()
+        {
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if(Changed != null)
+            {
+                Changed(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    class Observer
+    {
+        private ObserverSubject subject = new ObserverSubject();
+        private string status;
+
+        public void Test()
+        {
+            subject.Changed += Subject_Changed;
+
+            Thread.Sleep(5000);
+        }
+
+        private void Subject_Changed(object sender, EventArgs e)
+        {
+            status = "Subject updated at " + DateTime.Now;
+            Debug.WriteLine(status);
         }
     }
 }
